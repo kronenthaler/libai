c6bd585e5e5a5b328cc494c669f1151d721cd63e
Federico Vera <dktcoding@gmail.com>
2016-12-11 01:21:49 -0300

    Remove trailing spaces LVQ

--- src/libai/nn/supervised/LVQ.java
+++ src/libai/nn/supervised/LVQ.java
@@ -2,17 +2,17 @@
  * MIT License
  *
  * Copyright (c) 2009-2016 Ignacio Calderon <https://github.com/kronenthaler>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -41,7 +41,7 @@ import libai.nn.unsupervised.Competitive;
  */
 public class LVQ extends Competitive {
 	private static final long serialVersionUID = 6603129562167746698L;
-	
+
 	protected Matrix W2;
 	protected int subclasses;
 
@@ -183,10 +183,10 @@ public class LVQ extends Competitive {
 
 	/**
 	 * Deserializes an {@code LVQ}
-	 * 
+	 *
 	 * @param path Path to file
 	 * @return Restored {@code LVQ instance}
-	 * @see libai.nn.NeuralNetwork#save(java.lang.String) 
+	 * @see libai.nn.NeuralNetwork#save(java.lang.String)
 	 */
 	public static LVQ open(String path) {
 		try (FileInputStream fis = new FileInputStream(path);
